476c445263175c009a7fb5c8cccb86903805d801
Training_b6b.01.13 <Training_b6b.01.13@accenture.com>
2016-06-15 13:41:44 +0530

    npm

---
+++ yeoman/app/scripts/controllers/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoApp.controller:UserCtrl
+ * @description
+ * # UserCtrl
+ * Controller of the todoApp
+ */
+angular.module('todoApp')
+  .controller('UserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
